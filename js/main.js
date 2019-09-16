de88e25517580f2c09cde9ba2fd8764ffdde1ca7
Pavel <timohyk96@gmail.com>
2019-09-16 23:04:16 +0300

    changed errors

---
+++ js/main.js
@@ -0,0 +1,71 @@
+(function() {
+    let widthWindow = document.documentElement.clientWidth;
+    let headerInform = $('.header_information__wrapper');
+    let headerLang = $('.change_lang__wrapper');
+
+    if (widthWindow <= 768) {
+        $('#navbar').append(headerInform);
+        $('#navbar').append(headerLang);
+    }
+
+    function removeError() {
+        $('#main-form').removeClass('error')
+    }
+
+    function addError() {
+        $('#main-form').addClass('error');
+    }
+
+	$('#next_question').click(function(event){
+        event.preventDefault();
+        let firstCheckboxs = $(".first_checkbox");
+        let firstChecked = false;
+        $(firstCheckboxs).each(function () {
+            if ($(this).prop('checked')) {
+                firstChecked = true;
+            } 
+        }); 
+
+        if(firstChecked === false) {
+            addError();
+            setTimeout(removeError,800);
+        } else {
+            $('#first-step').slideUp("slow");
+            $('#second-step').slideDown("slow");
+        }  
+    });
+
+    $('.form_button__next').click(function(event){
+        event.preventDefault();
+        let secondCheckboxs = $(".second_checkbox");
+        let secondChecked = false;
+        $(secondCheckboxs).each(function () {
+            if ($(this).prop('checked')) {
+                secondChecked = true;
+            } 
+        }); 
+
+        if(secondChecked === false) {
+            addError();
+            setTimeout(removeError,800);
+        } else {
+            $('#second-step').slideUp("slow");
+            $('#third-step').slideDown("slow");
+            setTimeout(openStepFourth, 3000);
+        }
+    });
+
+    $('.form_button__back').click(function(event){
+        event.preventDefault();
+        $('#second-step').slideUp("slow");
+        $('#first-step').slideDown("slow");
+    });
+
+    function openStepFourth() {
+        $('#third-step').slideUp("slow");
+        $('#fourth-step').slideDown("slow");
+    }
+
+    
+})();
+
---
+++ js/timer.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function() {
+    jQuery(".eTimer").eTimer({
+        etType: 2, etDate: "15.09.2019.0.0", etSep: ":", etFontFamily: "PF Din Text Cond Pro", etTextColor: "white", etPaddingTB: 0, etPaddingLR: 0, etLastUnit: 4, etNumberFontFamily: "PF Din Text Cond Pro", etNumberSize: 48, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 0
+    });
+});
